cdc342f23ae8dca61477d98170feb74649db4665
ayushi.mittal1 <ayushi.mittal1@tothenew.com>
2024-09-17 20:19:54 +0530

    doc app system

---
+++ client/src/App.js
@@ -0,0 +1,76 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { useSelector } from "react-redux";
+import Spinner from "./components/Spinner";
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
+import ApplyDoctor from "./pages/ApplyDoctor";
+import Profile from "./pages/Profile";
+import Appointments from "./pages/Appointments";
+function App() {
+  const { loading } = useSelector((state) => state.alerts);
+  return (
+    <>
+      <BrowserRouter>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <HomePage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/apply-doctor"
+              element={
+                <ProtectedRoute>
+                  <ApplyDoctor />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <PublicRoute>
+                  <Login />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <PublicRoute>
+                  <Register />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/appointments"
+              element={
+                <ProtectedRoute>
+                  <Appointments />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        )}
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Appointments.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Layout from "./../components/Layout";
+import moment from "moment";
+import { Table } from "antd";
+
+const Appointments = () => {
+  const [appointments, setAppointments] = useState([]);
+
+  const getAppointments = async () => {
+    // api call to get all the appointment data
+  };
+
+  useEffect(() => {
+    getAppointments();
+  }, []);
+
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "_id",
+    },
+    {
+      title: "Date & Time",
+      dataIndex: "date",
+      render: (text, record) => (
+        <span>
+          {moment(record.date).format("DD-MM-YYYY")} &nbsp;
+          {moment(record.time).format("HH:mm")}
+        </span>
+      ),
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+    },
+  ];
+
+  return (
+    <Layout>
+      <h1>Appoinmtnets Lists</h1>
+      <Table columns={columns} dataSource={appointments} />
+    </Layout>
+  );
+};
+
+export default Appointments;
